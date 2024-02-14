38a3fd2223e50017cbc5e273e757ef88d1dacd71
DhairyaYadav <137642502+DhairyaYadav@users.noreply.github.com>
2024-02-15 00:56:30 +0530

    fixed logout

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -137,8 +137,8 @@ const logoutUser = asyncHandler(async(req,res) => {
     User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1 //this removes the field
             }
         },
         {
